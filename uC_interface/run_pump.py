b927247ce9ee910da1f701b9dfce58941a023827
PabloBelmonte <pnabelmonte@gmail.com>
2020-05-31 15:56:14 -0300

    Adding arg to function

---
+++ uC_interface/run_pump.py
@@ -0,0 +1,18 @@
+import time
+import os
+
+# Code to run the pump on the uController
+
+delay = 5
+
+print()
+print(f'Running pump for time equal to: {delay} seconds...')
+print()
+
+time.sleep(delay)
+
+print()
+print('Pump ready.')
+print()
+
+# exit()
