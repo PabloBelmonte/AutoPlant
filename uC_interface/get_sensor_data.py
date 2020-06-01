afd485c216d30ba73b30001e3ae8ed0ec0e42b8d
PabloBelmonte <pnabelmonte@gmail.com>
2020-06-01 00:38:09 -0300

    test

--- uC_interface/get_sensor_data.py
+++ uC_interface/get_sensor_data.py
@@ -1,17 +1,25 @@
 import time
 import json
-
+import csv
+import os
+import numpy as np
 
 def get_temperature():
-    temp = 25
+    mu, sigma = 25, 0.1 # mean and standard deviation
+    temp = np.random.normal(mu, sigma, 1)
+    temp = temp[0]
     return temp
 
 def get_ph():
-    ph = 7
+    mu, sigma = 7, 0.1 # mean and standard deviation
+    ph = np.random.normal(mu, sigma, 1)
+    ph = ph[0]
     return ph
 
 def get_humidity():
-    humidity = 80
+    mu, sigma = 80, 0.1 # mean and standard deviation
+    humidity = np.random.normal(mu, sigma, 1)
+    humidity = humidity[0]
     return humidity
 
 # Code to get sensor data on the uController
@@ -38,11 +46,20 @@ json_object = json.dumps(dictionary, indent = 4)
 with open("uC_interface/sensor.json", "w") as outfile:
     outfile.write(json_object)
 
+print('Sensors data ready!\n')
+
+if os.path.isfile('uC_interface/sensorRecordData.csv'):
+    # Apend data to csv file
+    with open("uC_interface/sensorRecordData.csv", "a") as csv_file:
+        writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        writer.writerow([str(temperature), str(ph), str(humidity)])
+else:
+    with open("uC_interface/sensorRecordData.csv", "w") as csv_file:
+        writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(['"temperature"', '"ph"', '"humidity"'])
+        writer.writerow([str(temperature), str(ph), str(humidity)])
+    
+
+
 print('Sensors data ready!')
 print('--------------------------')
-
-# os.environ('TEMPERATURE') = str(temperature)
-# os.environ('PH') = str(ph)
-# os.environ('HUMIDITY') = str(humidity)
-
-# exit()
